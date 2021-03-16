294afb357258bb3ea8f22a6378261fe7034058d3
Lekshmi M B <lekshmi.mb@iiitmk.ac.in>
2021-03-16 11:22:29 +0530

    Removed file upload for login

--- CookiejarClient/routes/CookiejarClient.js
+++ CookiejarClient/routes/CookiejarClient.js
@@ -1,13 +1,14 @@
-const {createHash} = require('crypto')
-const {CryptoFactory, createContext } = require('sawtooth-sdk/signing')
+const { createHash } = require('crypto')
+const { CryptoFactory, createContext } = require('sawtooth-sdk/signing')
 const protobuf = require('sawtooth-sdk/protobuf')
-const fs = require('fs')
+// const fs = require('fs')
 const fetch = require('node-fetch');
-const {Secp256k1PrivateKey} = require('sawtooth-sdk/signing/secp256k1')	
-const {TextEncoder, TextDecoder} = require('text-encoding/lib/encoding')
+const { Secp256k1PrivateKey } = require('sawtooth-sdk/signing/secp256k1')
+const { TextEncoder, TextDecoder } = require('text-encoding/lib/encoding')
 
 
-FAMILY_NAME='cookiejar'
+const FAMILY_NAME = "cookiejar";
+const FAMILY_VERSION = "1.0";
 
 function hash(v) {
   return createHash('sha512').update(v).digest('hex');
@@ -15,109 +16,98 @@ function hash(v) {
 
 
 
-class CookiejarClient{
-  constructor(Key){
-     const context = createContext('secp256k1');
-     const secp256k1pk = Secp256k1PrivateKey.fromHex(Key.trim());
-     this.signer = new CryptoFactory(context).newSigner(secp256k1pk);
-     this.publicKey = this.signer.getPublicKey().asHex();
-     this.address = this.get_address(this.publicKey);
-      console.log("Storing at: " + this.address);
-  } 
+class CookiejarClient {
+  constructor(Key) {
+    const context = createContext('secp256k1');
+    const secp256k1pk = Secp256k1PrivateKey.fromHex(Key.trim());
+    this.signer = new CryptoFactory(context).newSigner(secp256k1pk);
+    this.publicKey = this.signer.getPublicKey().asHex();
+    this.address = hash(FAMILY_NAME).substr(0, 6) + hash(this.publicKey).substr(0, 64);
+    console.log("Storing at: " + this.address);
+  }
 
-get_address(publicKey){
-   var Address= hash("cookiejar").substr(0, 6) + hash(publicKey).substr(0, 64);
-   return Address
-}
+  async send_data(action, values) {
 
- async send_data(action,values){
-  console.log('senddata')
-  var payload = ''
-  var address = this.address;
-  console.log("output "+address)
+    var inputAddressList = [this.address];
+    var outputAddressList = [this.address];
+    var payload = action + "," + values;
+    var encode = new TextEncoder('utf8');
+    const payloadBytes = encode.encode(payload)
+    const transactionHeaderBytes = protobuf.TransactionHeader.encode({
+      familyName: FAMILY_NAME,
+      familyVersion: FAMILY_VERSION,
+      inputs: inputAddressList,
+      outputs: outputAddressList,
+      signerPublicKey: this.signer.getPublicKey().asHex(),
+      nonce: "" + Math.random(),
+      batcherPublicKey: this.signer.getPublicKey().asHex(),
+      dependencies: [],
+      payloadSha512: hash(payloadBytes),
 
-  var inputAddressList = [address];
-  var outputAddressList = [address];
-  payload = action + ","+ values;
-  var encode =new TextEncoder('utf8');
-  const payloadBytes = encode.encode(payload)
-  console.log("haii")
-  const transactionHeaderBytes = protobuf.TransactionHeader.encode({
-    familyName: 'cookiejar',
-    familyVersion: '1.0',
-    inputs: inputAddressList,
-    outputs: outputAddressList,
-    signerPublicKey: this.signer.getPublicKey().asHex(),
-    nonce: "" + Math.random(),
-    batcherPublicKey: this.signer.getPublicKey().asHex(),
-    dependencies: [],
-    payloadSha512: hash(payloadBytes),
+    }).finish();
 
-  }).finish();
+    const transaction = protobuf.Transaction.create({
+      header: transactionHeaderBytes,
+      headerSignature: this.signer.sign(transactionHeaderBytes),
+      payload: payloadBytes
+    });
+    const transactions = [transaction];
+    const batchHeaderBytes = protobuf.BatchHeader.encode({
+      signerPublicKey: this.signer.getPublicKey().asHex(),
+      transactionIds: transactions.map((txn) => txn.headerSignature),
+    }).finish();
 
-  const transaction = protobuf.Transaction.create({
-    header: transactionHeaderBytes,
-    headerSignature: this.signer.sign(transactionHeaderBytes),
-    payload: payloadBytes
-  });
-  const transactions = [transaction];
-  const  batchHeaderBytes = protobuf.BatchHeader.encode({
-    signerPublicKey: this.signer.getPublicKey().asHex(),
-    transactionIds: transactions.map((txn) => txn.headerSignature),
-  }).finish();
+    const batchSignature = this.signer.sign(batchHeaderBytes);
+    const batch = protobuf.Batch.create({
+      header: batchHeaderBytes,
+      headerSignature: batchSignature,
+      transactions: transactions,
+    });
 
-  const batchSignature = this.signer.sign(batchHeaderBytes);
-  const batch = protobuf.Batch.create({
-    header: batchHeaderBytes,
-    headerSignature: batchSignature,
-    transactions: transactions,
-  });
+    const batchListBytes = protobuf.BatchList.encode({
+      batches: [batch]
+    }).finish();
+    this._send_to_rest_api(batchListBytes);
 
-  const batchListBytes = protobuf.BatchList.encode({
-    batches: [batch]
-  }).finish();
-   this._send_to_rest_api(batchListBytes);	
-
-}
+  }
 
 
-async _send_to_rest_api(batchListBytes){
-        
+  async _send_to_rest_api(batchListBytes) {
+
     if (batchListBytes == null) {
-      try{
-      var geturl = 'http://rest-api:8008/state/'+this.address
-      console.log("Getting from: " + geturl);
-       let response =await fetch(geturl, {
-        method: 'GET',
-      })
-      let responseJson =await response.json();
+      try {
+        var geturl = 'http://rest-api:8008/state/' + this.address
+        console.log("Getting from: " + geturl);
+        let response = await fetch(geturl, {
+          method: 'GET',
+        })
+        let responseJson = await response.json();
         var data = responseJson.data;
-        var amount = new Buffer(data, 'base64').toString();
-        return amount;
+        var cookieCount = new Buffer(data, 'base64').toString();
+        return cookieCount;
       }
-      catch(error) {
+      catch (error) {
         console.error(error);
-      }	
+      }
     }
-    else{
-      console.log("new code");
-      try{
-     resp =await fetch('http://rest-api:8008/batches', {
-  method: 'POST',
-        headers: {
-    'Content-Type': 'application/octet-stream'
-        },
-        body: batchListBytes
-         })
-           console.log("response", resp);
-        }
-         catch(error) {
-           console.log("error in fetch", error);
-         
-       } 
-   }
+    else {
+      try {
+        resp = await fetch('http://rest-api:8008/batches', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/octet-stream'
+          },
+          body: batchListBytes
+        })
+        console.log("response", resp);
+      }
+      catch (error) {
+        console.log("error in fetch", error);
 
-}
+      }
+    }
+
+  }
 
 
-}module.exports.CookiejarClient = CookiejarClient;
+} module.exports.CookiejarClient = CookiejarClient;
--- CookiejarClient/routes/index.js
+++ CookiejarClient/routes/index.js
@@ -1,60 +1,58 @@
 var express = require('express');
 var router = express.Router();
-var {CookiejarClient}  = require('./CookiejarClient');
+var { CookiejarClient } = require('./CookiejarClient');
 var fs = require('fs');
 
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'KBA' });
 });
 
-router.get('/bake',(req,res)=>{
+router.get('/bake', (req, res) => {
   res.render('Bake');
 });
 
-router.get('/eat',(req,res)=>{
+router.get('/eat', (req, res) => {
   res.render('Eat');
 });
 
-router.get('/count',(req,res)=>{
+router.get('/count', (req, res) => {
   res.render('Count');
 });
 
-router.get('/home',(req,res)=>{
-  res.render('Home', { title: 'Sawtooth'});
+router.get('/home', (req, res) => {
+  res.render('Home', { title: 'Sawtooth' });
 });
 
-router.post('/',(req,res)=>{
-  console.log("hello")
+router.post('/', (req, res) => {
   var Key = req.body.privateKey;
   var cookiejar_client = new CookiejarClient(Key);
-  res.send({ done:1, privatekey: Key, message: "your privatekey is "+ Key });
+  res.send({ done: 1, privatekey: Key, message: "your privatekey is " + Key });
 });
 
 
-router.post('/bake',(req,res,next)=>{
-  var count= req.body.cookie;
-  var pri_key= req.body.pri_key;
-  var  cookiejar_client = new CookiejarClient(pri_key);
-  cookiejar_client.send_data('bake',[count]);
-  res.send({message: "Bake " +count+ "Cookies"});
-
-});
-
-router.post('/eat',(req,res,next)=>{
-  var count= req.body.cookie;
-  var pri_key=req.body.pri_key;
+router.post('/bake', (req, res, next) => {
+  var count = req.body.cookie;
+  var pri_key = req.body.pri_key;
   var cookiejar_client = new CookiejarClient(pri_key);
-  cookiejar_client.send_data('eat',[count]);
-  res.send({message: "Eat " +count+ "Cookies"});
+  cookiejar_client.send_data('bake', [count]);
+  res.send({ message: "Bake " + count + " cookies request sent" });
+
 });
 
-router.post('/count', (req, res)=>{
-  var pri_key=req.body.pri_key;
+router.post('/eat', (req, res, next) => {
+  var count = req.body.cookie;
+  var pri_key = req.body.pri_key;
+  var cookiejar_client = new CookiejarClient(pri_key);
+  cookiejar_client.send_data('eat', [count]);
+  res.send({ message: "Eat " + count + " cookies request sent" });
+});
+
+router.post('/count', (req, res) => {
+  var pri_key = req.body.pri_key;
   const cookiejar_client = new CookiejarClient(pri_key);
-  const getYourBalance =  cookiejar_client._send_to_rest_api(null);
-  getYourBalance.then(result => {res.send({ balance: result});});
+  const getYourBalance = cookiejar_client._send_to_rest_api(null);
+  getYourBalance.then(result => { res.send({ balance: result }); });
 })
 module.exports = router;
